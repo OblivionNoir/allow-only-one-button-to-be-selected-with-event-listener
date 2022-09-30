4d7f30d77c5ac43deb31b5f294623da9d7a8e98a
Benjamin Donahue <79435245+OblivionNoir@users.noreply.github.com>
2022-09-30 12:52:50 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var players_array = [];
+var players = document.getElementsByClassName('clickable');
+players_array.push(players[0], players[1], players[2]);
+var active_added = false;
+//Character selection, working beautifully<3
+for (let i = 0; i < players_array.length; i++){
+    players_array[i].addEventListener('click', function selected(){
+        console.log("listeners added to players")
+        if (this.classList.contains('active')){ 
+            for (let i = 0; i < players_array.length; i++){
+                players_array[i].classList.remove('active') //remove all, not just one
+                active_added = false;
+
+            }//second for loop ends here    
+      
+        }else if (active_added == false){
+            this.classList.add('active')
+            active_added = true;
+            //access current index here, then pull up appropriate menu
+        }else{
+            console.log("what the fuck")
+        };
+    }//listener ends here
+)};//first for loop ends here
